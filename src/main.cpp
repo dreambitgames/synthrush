0703f805d79be48cecf7e9287a13750a08ec1458
Vardan2009 <70532109+Vardan2009@users.noreply.github.com>
2025-11-02 17:44:42 +0400

    Basic rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
+#include <raylib.h>
+
 #include <cstdio>
 
 int main() {
-    printf("Hello!!!\n");
+    InitWindow(800, 450, "[SYNTHRUSH]");
+
+    Camera3D camera{};
+    camera.position = {0, 10, 10};
+    camera.target = {0, 0, 0};
+    camera.up = {0, 1, 0};
+    camera.fovy = 45;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(WHITE);
+        BeginMode3D(camera);
+        DrawCube({0, 0, 0}, 2, 2, 2, RED);
+        EndMode3D();
+        EndDrawing();
+    }
+
     return 0;
 }
