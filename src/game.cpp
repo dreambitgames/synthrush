053f43f3d7bf6fb769d80cac75614b459ce01eec
Vardan2009 <70532109+Vardan2009@users.noreply.github.com>
2025-11-02 21:47:42 +0400

    Grid rendering

--- src/game.cpp
+++ src/game.cpp
@@ -2,11 +2,13 @@
 
 #include <raylib.h>
 
-synthrush::Game::Game() {
-    InitWindow(800, 450, "[SYNTHRUSH]");
+#include <cmath>
 
-    mCam.position = {0, 10, 10};
-    mCam.target = {0, 0, 0};
+synthrush::Game::Game() {
+    InitWindow(1920, 1080, "[SYNTHRUSH]");
+
+    mCam.position = {0, 5, -10};
+    mCam.target = {0, 5, 0};
     mCam.up = {0, 1, 0};
     mCam.fovy = 45;
     mCam.projection = CAMERA_PERSPECTIVE;
@@ -16,14 +18,35 @@ synthrush::Game::Game() {
 
 synthrush::Game::~Game() { CloseWindow(); }
 
-void synthrush::Game::Update(float dT) { UpdateCamera(&mCam, CAMERA_FREE); }
+void synthrush::Game::Update(float dT) {}
+
+static void DrawGroundGrid(float off) {
+    const float gridSpacing = 3.0f;
+    const int gridCount = 30;
+    const int gridCountSides = 10;
+    const float halfWidth = (gridCountSides / 2.0f) * gridSpacing;
+
+    off = fmodf(off, gridSpacing);
+
+    for (int i = -gridCountSides / 2; i <= gridCountSides / 2; ++i) {
+        float x = i * gridSpacing;
+        DrawLine3D({x, 0, -gridSpacing * gridCount}, {x, 0, gridSpacing * gridCount}, MAGENTA);
+    }
+
+    for (int i = -gridCount; i <= gridCount; ++i) {
+        float z = i * gridSpacing - off;
+        DrawLine3D({-halfWidth, 0, z}, {halfWidth, 0, z}, MAGENTA);
+    }
+}
 
 void synthrush::Game::Render(float dT) {
     BeginDrawing();
-    ClearBackground(WHITE);
+    ClearBackground(BLACK);
     BeginMode3D(mCam);
 
-    DrawCube({0, 0, 0}, 2, 2, 2, RED);
+    static float off = 0;
+    DrawGroundGrid(off);
+    off += dT * 10;
 
     EndMode3D();
     EndDrawing();
